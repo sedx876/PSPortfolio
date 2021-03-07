df24b18805305c67b6f4638b91363b7ee2b42d7a
sedx876 <swatkins876@gmail.com>
2021-03-07 06:55:35 -0500

    Overhaul so i could get JS color picker to work

---
+++ app.js
@@ -0,0 +1,76 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('main')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'main'){
+		document.getElementById('theme-style').href = 'main.css'
+	}
+
+  if(mode == 'wine'){
+		document.getElementById('theme-style').href = 'wine.css'
+	}
+
+	if(mode == 'iWantMyMTV'){
+		document.getElementById('theme-style').href = 'iWantMyMTV.css'
+	}
+
+	if(mode == 'brPastel'){
+		document.getElementById('theme-style').href = 'brPastel.css'
+	}
+
+	if(mode == 'goEagles'){
+		document.getElementById('theme-style').href = 'goEagles.css'
+	}
+
+  if(mode == 'mrAnderson'){
+		document.getElementById('theme-style').href = 'mrAnderson.css'
+	}
+
+  if(mode == 'whimsyShire'){
+		document.getElementById('theme-style').href = 'whimsyShire.css'
+	}
+
+  if(mode == 'houseStark'){
+		document.getElementById('theme-style').href = 'houseStark.css'
+	}
+
+  if(mode == 'goOrange'){
+		document.getElementById('theme-style').href = 'goOrange.css'
+	}
+
+  if(mode == 'waves'){
+		document.getElementById('theme-style').href = 'waves.css'
+	}
+
+  if(mode == 'cherryLimeade'){
+		document.getElementById('theme-style').href = 'cherryLimeade.css'
+	}
+
+  if(mode == 'subdued'){
+		document.getElementById('theme-style').href = 'subdued.css'
+	}
+
+  if(mode == 'coral'){
+		document.getElementById('theme-style').href = 'coral.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
